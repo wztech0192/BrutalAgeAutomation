30862fbd21161dfaf0b6b8a8add1e66f4c27572a
Wei J. Zheng <39068227+weijie0192@users.noreply.github.com>
2020-04-11 17:52:39 -0400

    Add files via upload

---
+++ src/events/handler/OtherEvents.java
@@ -0,0 +1,10 @@
+package events.register;
+
+import events.common.Event;
+
+import java.util.HashMap;
+
+public class OtherEvents {
+    public static void register(HashMap<String, Event> map) {
+    }
+}
